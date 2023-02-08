02edf7dfbcfd35b37567d90bf0e6ae7f8dc7b927
Alex Babenko <wertuniul@gmail.com>
2023-02-08 23:32:16 +0300

    JavaHomework2.2Fix

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int account = 323;
         int income = 1400;
-        int a = 100;
-        boolean threshold = income>1000;
-        int bonus = threshold ? income/a : 0;
-        System.out.println("Сумма на счете " + (account+income) + " Бонусных рублей " + bonus);
+        int coefficient = 100;
+        boolean threshold = income > 1000;
+        int bonus = threshold ? income / coefficient : 0;
+        System.out.println("Сумма на счете " + (account + income) + " Бонусных рублей " + bonus);
     }
 }
